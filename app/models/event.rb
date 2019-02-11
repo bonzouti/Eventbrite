262ca039ee4bf8de9ab142d5864b05d565292cf6
Aleksandra <a.tsyrlina@gmail.com>
2019-02-11 15:18:31 +0100

    seed length

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,19 +1,27 @@
 class Event < ApplicationRecord
-  validates :price,
-  presence: true
-  validates :user,
-  presence: true
-  validates :start_date,
-  presence:  true
-  validates :duration,
-  presence: true
-  validates :title,
-  presence: true
-  validates :description,
-  presence: true
-  validates :location,
-  presence: true
+  validates :price, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 1000 }
+  validates :user,  presence: true
+  validates :start_date, presence: true
+  validates :title, presence: true, length: { minimum: 5, maximum: 140 }
+  validates :description, presence: true, length: { minimum: 20, maximum: 1000 }
+  validates :location,  presence: true
+validates :duration, presence: true, numericality: { only_integer: true }, if: :multiple_of_5
   has_many :attendances
   has_many :users, through: :attendances
   belongs_to :user
+
+  validate :start_date_cannot_be_in_the_past
+  def start_date_cannot_be_in_the_past
+   errors.add(:start_date, "can't be in the past") if
+     !start_date.blank? and start_date < Time.now
+ end
+
+   def multiple_of_5
+    if duration % 5 == 0
+      return true
+    else
+      return false
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,10 @@ Attendance.destroy_all
    end
 puts "20 users ont été créés"
 20.times do
-    event = Event.create!(title: Faker::Pokemon.move, description: Faker::MostInterestingManInTheWorld.quote, user: User.all.sample, start_date: Faker::Date.forward(23), duration: rand(100..10000), price: rand(1..1000), location: Faker::HarryPotter.location)
+    event = Event.create!(title: Faker::Hipster.sentence(3), description: Faker::MostInterestingManInTheWorld.quote, user: User.all.sample, start_date: Faker::Date.forward(23), duration: rand(100..10000), price: rand(1..1000), location: Faker::HarryPotter.location)
    end
 puts "20 events ont été créés"
    30.times do
        attendance = Attendance.create!(stripe_customer_id: (0...5).map { ('a'..'z').to_a[rand(26)] }.join, user: User.all.sample, event: Event.all.sample)
    end
 puts "30 attendances ont été créées"
-
