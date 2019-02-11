b9de6eb6af068214041a785206e92e387a0e0d30
Aleksandra <a.tsyrlina@gmail.com>
2019-02-11 17:28:27 +0100

    mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   has_many :events
   has_many :attendances
   has_many :events, through: :attendances
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
